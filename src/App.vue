8be8b4142dad539267a8072251263fddf26ec5a7
Felix <jonfelixrico@gmail.com>
2020-05-06 17:40:46 +0800

    Add WebSockets support for notification receiving

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,14 @@
 </template>
 
 <script lang="ts">
-export default {
-  name: 'App'
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import notificationWsSvc from 'src/services/notification-ws.service'
+
+@Component
+export default class App extends Vue {
+  mounted () {
+    notificationWsSvc.connect()
+  }
 }
 </script>
---
+++ src/services/notification-ws.service.ts
@@ -0,0 +1,39 @@
+import SockJS from 'sockjs-client'
+import { RxStomp } from '@stomp/rx-stomp'
+import { Observable } from 'rxjs'
+import { IMessage } from '@stomp/stompjs'
+
+const { WS_URL, STOMP_DEST } = process.env
+
+export class NotificationWSService {
+  private stomp: RxStomp;
+
+  private _notifications$!: Observable <IMessage>
+
+  constructor () {
+    const socket = SockJS(WS_URL || '/notification-ws')
+    const stomp = this.stomp = new RxStomp()
+
+    stomp.configure({
+      debug: msg => console.log(msg),
+      webSocketFactory: () => socket
+    })
+
+    this._notifications$ = this.stomp.watch(STOMP_DEST || '/receive/notifications')
+  }
+
+  connect () {
+    this.stomp.activate()
+  }
+
+  disconnect () {
+    this.stomp.deactivate()
+  }
+
+  get notifications$ (): Observable<IMessage> {
+    return this._notifications$
+  }
+}
+
+const notificationWsSvc = new NotificationWSService()
+export default notificationWsSvc
